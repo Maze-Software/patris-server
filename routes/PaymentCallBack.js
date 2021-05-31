b522f67fed071e82615cc158876d9b1b7e83627f
Fırat Doğan <firatdogan0@gmail.com>
2021-05-31 21:16:11 +0300

    get

--- routes/PaymentCallBack.js
+++ routes/PaymentCallBack.js
@@ -4,7 +4,7 @@ const mongoose = require('mongoose');
 const route = express.Router();
 const { paymentCallBack } = require('../Controllers/UserController');
 
-route.post('/', async (req, res) => {
+route.get('/', async (req, res) => {
     paymentCallBack(req, res);
 });
 
